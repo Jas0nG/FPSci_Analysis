a89a3e8f172513ecff2aada2d2e312fe21c868aa
WuTianchen <wutianchen@zerozero.cn>
2023-07-19 17:22:12 +0800

    [demo] - First demo of dataprocess

---
+++ FPSci_Importer/Importer.py
@@ -0,0 +1,253 @@
+import sqlite3
+import math
+from datetime import datetime
+
+IN_LOG_TIME_FORMAT = '%Y-%m-%d %H:%M:%S.%f'
+
+class Trial:
+    def __init__(self, sessionId, blockId, tastId, tastIndex, trialId, trialIndex, startTime, endTime, duaration, taskExecTime, destroyedTargets, totalTargets, index=-1):
+        self.sessionId = sessionId
+        self.blockId = blockId
+        self.tastId = tastId
+        self.tastIdx = tastIndex
+        self.trialId = trialId
+        self.trialIdx = trialIndex
+        self.startTime = startTime
+        self.endTime = endTime
+        self.duaration = duaration
+        self.taskExecTime = taskExecTime
+        self.destroyedTargets = destroyedTargets
+        self.totalTargets = totalTargets
+        self.index = index
+
+    def show(self):
+        print('sessionId',self.sessionId)
+        print('blockId',self.blockId)
+        print('tastId',self.tastId)
+        print('tastIdx',self.tastIdx)
+        print('startTime',self.startTime)
+        print('endTime',self.endTime)
+        print('duaration',self.duaration)
+        print('taskExecTime',self.taskExecTime)
+        print('destroyedTargets',self.destroyedTargets)
+        print('totalTargets',self.totalTargets)
+        print('index',self.index)
+
+class PlayerAction:
+    def __init__(self, t, pos_az, pos_el, pos_x, pos_y, pos_z, event, state, targetId=None):
+        self.time = datetime.strptime(t, IN_LOG_TIME_FORMAT)
+        self.view_az = float(pos_az)
+        self.view_el = float(pos_el)
+        self.pos_x = float(pos_x)
+        self.pos_y = float(pos_y)
+        self.pos_z = float(pos_z)
+        self.event = event
+        self.state = state
+        self.targetId = targetId
+
+class Target:
+    def __init__(self, targetId, trialId, tType, tParams, model):
+        self.id = targetId
+        self.trial = trialId
+        self.type = tType
+        self.params = tParams
+        self.model = model
+
+class QuestionResponse:
+    def __init__(self, sessionId, question, response):
+        self.sessId = sessionId
+        self.question = question
+        self.response = response
+
+class FrameInfo:
+    def __init__(self, time, sdt):
+        self.time = time
+        self.sdt = float(sdt)
+
+class Event:
+    def __init__(self, time, eventType):
+        self.time = time
+        self.type = eventType
+
+class Click:
+    def __init__(self, time, azim, elev, hit, clicktophoton):
+        self.time = time
+        self.azim = azim
+        self.elev = elev
+        self.hit = hit
+        self.clicktophoton = clicktophoton
+
+class Importer:
+    """Simple class for importing data from abstract-fps results files"""
+
+    def __init__(self, dbName):
+        self.db = sqlite3.connect(dbName)
+
+    ######################################################
+    # Generic Functions for DB operations
+    ######################################################
+    def queryDb(self, query):
+        """Simple method to query the db"""
+        c = self.db.cursor()
+        c.execute(query)
+        return c.fetchall()
+
+    def getTableRows(self, tableName):
+        """Get all rows from a particular table"""
+        return self.queryDb('SELECT * FROM {0}'.format(tableName))
+
+    ######################################################
+    # FPSci Specific Tools
+    ######################################################
+    def getTrials(self):
+        """Get all trials from the trials table"""
+        trials = []
+        tidx = {}
+        for row in self.getTableRows('Trials'):
+            if row[0] in tidx.keys(): tidx[row[0]] += 1
+            else: tidx[row[0]] = 0
+            trials.append(Trial(row[0], row[1], row[2], row[3], row[4], row[5], row[6],  row[7], row[8], row[9], row[10], row[11], tidx[row[0]]))
+        return trials
+
+    def getCondIds(self):
+        """Get a list of trial ids from the trials table"""
+        ids = []
+        for trial in self.getTrials(): ids.append(trial.id)
+        return ids
+
+    def getTrialsById(self, condId):
+        """Get a particular trial(s) from the trials table"""
+        rows = self.queryDb('SELECT * FROM Trials WHERE [condition_ID] = {0}'.format(condId))
+        if(len(rows) > 1):
+            out = []
+            for row in rows: out.append(Trial(row[0], row[1], row[2], row[3], row[4], row[5], row[6]))
+            return out
+        elif(len(rows) == 1): 
+            row = rows[0]
+            return Trial(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
+        return None
+
+    def getEvents(self):
+        """Get all events from the events table"""
+        events = []
+        for row in self.getTableRows('event_log'): events.append(Event(row[0], row[1]))
+        return events
+
+    def getTrialTargetPositionsXYZ(self, trial, targetId=None):
+        query = "SELECT * FROM Target_Trajectory WHERE [time] <= \'{0}\' AND [time] >= \'{1}\'".format(trial.endTime, trial.startTime)
+        if targetId is not None: query += ' AND [target_id] = \'{2}\''.format(targetId)
+        positions = {}
+        for row in self.queryDb(query): 
+            if row[1] not in positions.keys(): positions[row[1]] = [[row[3], row[4], row[5]]]
+            else: positions[row[1]].append([row[3], row[4], row[5]])
+        return positions
+
+    def getTargetPositionsXYZ(self, condId, trialIdx=0):
+        """Get all target positions (for a given condition id) from the Target_Trajectory table"""
+        trial = self.getTrialsById(condId)
+        if type(trial) is list: trial = trial[trialIdx]
+        return self.getTrialTargetPositionsXYZ(trial)
+
+    # Helper function for convering lists of XYZ to azim/elev
+    def toAzimElev(self, target_xyz):
+        """Helper for converting lists of XYZ coordinates to polar"""
+        positions = {}
+        for targetId in target_xyz.keys():
+            positions[targetId] = []
+            for [x,y,z] in target_xyz[targetId]:
+                x = float(x)
+                y = float(y)
+                z = float(z)
+                r = math.sqrt(x**2+y**2+z**2)
+                azim = math.atan(z/x)
+                elev = math.asin(y/r)
+                positions[targetId].append([r, azim, elev])
+        return positions
+
+    def getTrialTargetPositionsAzimElev(self, trial):
+        positionsXYZ = self.getTrialTargetPositionsXYZ(trial)
+        return self.toAzimElev(positionsXYZ)
+
+    def getTargetPositionsAzimElev(self, condId, trialIdx=0):
+        """Get all target positions (for a given condition id) as azim/elev from Target_Trajectory table"""
+        positionsXYZ = self.getTargetPositionsXYZ(condId, trialIdx)
+        return self.toAzimElev(positionsXYZ)
+    
+    def getTrialPlayerActions(self, trial):
+        """Get all player actions from a particular trial"""
+        actions = []
+        for row in self.queryDb("SELECT * FROM Player_Action WHERE [time] <= \'" + trial.endTime + "\' AND [time] >= \'" + trial.startTime + "\'"): 
+            actions.append(PlayerAction(row[0], row[1], row[2], row[3], row[4] , row[5], row[6], row[7], row[8]))
+        return actions
+
+    def getPlayerActions(self, condId, trialIdx=0):
+        """Get player actions (for a given condition id) from the Player_Action table"""
+        trial = self.getTrialsById(condId)
+        if type(trial) is list: trial = trial[trialIdx]
+        return self.getTrialPlayerActions(trial)
+
+    def getClicks(self):
+        """Get click information from the database"""
+        clicks = []
+        for [t,event,azim,elev] in self.queryDb('SELECT * from Player_Action WHERE [event] = \'hit\' OR [event] = \'miss\''):
+            query = 'SELECT latency from click_latencies WHERE time >= \'{0}\' ORDER BY time ASC'.format(t)
+            c2p = self.queryDb(query)
+            if len(c2p) == 0: 
+                c2ptime = None
+            else: 
+                c2ptime = c2p[0][0]
+            clicks.append(Click(t, azim, elev, event == 'hit', c2ptime))
+        return clicks
+
+    def getRowTarget(self, row):
+        """Get a target from a row returned from the database"""
+        # Dealing with a parametric target, get params
+        if(row[2] == 'parametrized'): 
+            params = {}
+            params["refresh_rate"] = row[3]
+            params["added_frame_lag"] = row[4]
+            params["min_ecc_h"] = row[5]
+            params["min_ecc_v"] = row[6]
+            params["max_ecc_h"] = row[7]
+            params["max_ecc_v"] = row[8]
+            params["min_speed"] = row[9]
+            params["max_speed"] = row[10]
+            params["min_motion_change_period"] = row[11]
+            params["max_motion_change_period"] = row[12]
+            params["jump_enabled"] = row[13]       
+        return Target(row[0], row[1], row[2], params, row[14])
+
+    def getTarget(self, targetId):
+        """Get a single target by its (unqiue) id"""
+        row = self.queryDb('SELECT * from Targets WHERE target_id = \'{0}\''.format(targetId))
+        if len(row) == 0: return None
+        return self.getRowTarget(row[0])
+
+    def getTrialTargets(self, trialId = None):
+        """Get a list of targets based on a trial ID"""
+        query = 'SELECT * from Targets '
+        if trialId is not None: query += 'WHERE trial_id = \'{0}\''.format(trialId)
+        rows = self.queryDb(query)
+        targets = []
+        for row in rows: targets.append(self.getRowTarget(row))
+
+    def getQuestionResponses(self, sessId = None):
+        """"Get the questions and responses as a list"""
+        query = 'SELECT * from Questions '
+        if sessId is not None: query += 'WHERE Session = \'{0}\''
+        rows = self.queryDb(query)
+        questions = []
+        for row in rows: questions.append(QuestionResponse(row[0], row[1], row[2]))
+        return questions
+
+    def getFrameInfo(self):
+        """Get the frame info table as a list"""
+        frames = []
+        for row in self.getTableRows('Frame_Info'):
+            frames.append(FrameInfo(row[0], row[1]))
+        return frames
+
+    def parseTime(self, timeStr):
+        return datetime.strptime(timeStr, IN_LOG_TIME_FORMAT)
+        
+
---
+++ velocity.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+from FPSci_Importer.Importer import Importer
+import sys
+import numpy as np
+
+if len(sys.argv) < 2: raise Exception("Provide db filename as input!")
+
+# Get the database
+db = Importer(sys.argv[1])
+# db = Importer('test.db')
+trials = db.getTrials()
+
+
+clicks = db.getClicks()
+# Analyze each trial in the results
+for trial in trials:
+    # Get target trajectory and player actions from the db
+    trajectories = db.getTrialTargetPositionsAzimElev(trial)  # This is now a dictionary
+    actions = db.getTrialPlayerActions(trial)
+    print(trial.index)
+
+
+
+# 提取视角方位角和仰角的时间序列数据
+time = [action.time for action in actions]
+azimuth = [action.view_az for action in actions]
+elevation = [action.view_el for action in actions]
+
+# 计算合成角速度
+angular_velocity = []
+for i in range(1, len(time)):
+    dt = (time[i] - time[i-1]).total_seconds()  # 计算时间间隔（单位：秒）
+    d_azimuth = azimuth[i] - azimuth[i-1]  # 方位角变化量
+    d_elevation = elevation[i] - elevation[i-1]  # 仰角变化量
+    # 计算角速度（合成角速度）
+    velocity = np.sqrt((d_azimuth / dt)**2 + (d_elevation / dt)**2)
+    angular_velocity.append(velocity)
+
+# 绘制合成角速度-时间曲线
+plt.plot(time[1:], angular_velocity)
+plt.xlabel('Time')
+plt.ylabel('Angular Velocity')
+plt.title('Player View Angular Velocity')
+plt.show()
